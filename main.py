854c0a260cf3ba260a1381a299c12e24796a5780
hiz <motion@j-hiz.com>
2018-05-17 12:55:13 +0000

    - adde confighandler to main

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ import modules.groveController as controller
 from time import sleep
 from modules.display import turnOffDisplay
 from modules.rpisocket.socket import checkConnection
+from modules.configHandler import isFirstRun
 
 #Main app
 print('run')
 #init
 def init():
     print('-[init]initialization')
+    isFirstRun()
     start()
 
 #start
