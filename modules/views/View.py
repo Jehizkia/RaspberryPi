282727f5bed6c66be92c5544334495455a9817ed
hiz <motion@j-hiz.com>
2018-05-20 10:55:59 +0000

    split up View into to submodules

--- modules/views/View.py
+++ modules/views/View.py
@@ -1,11 +1,6 @@
 from abc import ABCMeta, abstractmethod
 from time import sleep
 import grove_rgb_lcd as lcd
-import sys,os
-sys.path.insert(1, os.path.join(sys.path[0], './modules'))
-#import sensor
-#sys.path.append('/home/pi/Desktop/ictlab/rpiGit/RaspberryPi/modules')
-from sensor import getDhtData
 
 class BaseView(object):
     __metaclass__ = ABCMeta
@@ -26,29 +21,6 @@ class BaseView(object):
     @abstractmethod
     def display(self):
         pass
-
-class TempView(BaseView):
-    def getTempHum(self):
-        self.temperature = getDhtData()[0]
-        self.humidity = getDhtData()[1]
-        self.updateText('Temp:' + str(self.temperature) + 'C\nHum:' + str(self.humidity))
-
-    def changeColorByTemp():
-        pass
-    
-    def display(self):
-        self.getTempHum()
-        lcd.setText(self.text)
-        self.setColor()
-    
-
-class NotifyView(BaseView):
-    def getNotification(self):
-        self.message = ''
-
-    def display(self):
-        lcd.setText(self.text)
-        self.setColor()
-    
+   
 
 
