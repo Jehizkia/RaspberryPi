1ace064fb0e33bfee373253cf13148180758ddcb
jehizkia <0893430@hr.nl>
2018-06-04 21:17:28 +0000

    Setup for Dispatcher

---
+++ modules/front_controller.py
@@ -0,0 +1,23 @@
+class Dispatcher(object):
+
+    def __init__(self):
+        pass
+
+    def dispatch(self, request):
+        pass
+
+class RequestController(object):
+
+    def __init__(self):
+        pass
+
+    def dispatch_request(self, request):
+        pass
+
+
+class Request(object):
+
+    def __init__(self, request):
+        pass
+
+    
